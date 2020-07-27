5b5c692d65c6ff2542f054972057486fdf2103c5
Andrei Beraru <raberaru@Andreis-MacBook-Pro.local>
2020-07-27 08:29:51 +0300

    Removed vector icons

--- src/components/ContactItem.tsx
+++ src/components/ContactItem.tsx
@@ -4,11 +4,22 @@ import {View, Text, StyleSheet} from 'react-native';
 const ContactItem = () => {
   return (
     <View>
-      <Text>This is the modal!</Text>
+      <View style={styles.icon}></View>
+      <View>
+        <Text>Name</Text>
+        <Text>+40 763 654 814</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  icon: {
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    backgroundColor: '#ddd',
+  },
+});
 
 export default ContactItem;
